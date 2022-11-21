b76a5269d6c34d797c9f52917a9379f4f19aa388
lukaafff <luizagarcia2405@gmail.com>
2022-11-21 18:59:16 -0300

    update

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.onload = function(){
     const velocidade = 1; //qunatas casas a cobrinha anda 
 
     var velocidadeX = velocidadeY =0;
-    var posicaoX = 10; //posição onde inicia a cobrinha
-    var posicaoY = 15; //posição onde inicia a cobrinha
+    var posicaoX = 11; //posição onde inicia a cobrinha
+    var posicaoY = 14; //posição onde inicia a cobrinha
     var tamanhoPeca = 30;
     var quantidadePeca = 20;
     var macaX = macaY = 15; //posição inicial da maça
@@ -16,36 +16,39 @@ window.onload = function(){
     var pontuacao = 0;
 
     var rastro = []; //o reastro começa com a array vazia, pois não tem nenhum elemento
-    calda = 5; //calda da cobrinha começa com tamanho 5
+    calda = 2; //calda da cobrinha começa com tamanho 5
 
     function jogo (){
-        pontuacaobox.innerHTML = "Pontuação: " + pontuacao;
+        pontuacaobox.innerHTML = "Pontuação: " + pontuacao; //pontução começa zerada
         posicaoX += velocidadeX; //cobrinha começa parada
         posicaoY += velocidadeY; //cobrinha começa parada
 
-        if (posicaoX < 0){
+/*
+        //if para passar de um lado para o outro
+        if (posicaoX < 0){ //quando for pra esquerda aparece na direita
             posicaoX = quantidadePeca - 1;
         }
-        if (posicaoX > quantidadePeca - 1){
+        if (posicaoX > quantidadePeca - 1){ //quando for pra direita aparece na esquerda
             posicaoX = 0;
         }
-         if (posicaoY < 0){
+         if (posicaoY < 0){  //quando for pra cima aparece em baixo
             posicaoY = quantidadePeca - 1;
          }
-         if (posicaoY > quantidadePeca - 1){
+         if (posicaoY > quantidadePeca - 1){ //quando for pra baixo aparece em cima
             posicaoY = 0;
          }
-    
+*/
+        
         //tela
-        ctx.fillStyle = "black"; //estilo de preechimento
+        ctx.fillStyle = "#ff8585"; //estilo de preechimento
         ctx.fillRect(0,0, tela.width, tela.height); //forma um retângulo e pinta toda a tela
 
         //maça
-        ctx.fillStyle = "red"; //estilo de preechimento
+        ctx.fillStyle = "#b53535"; //estilo de preechimento
         ctx.fillRect(macaX*tamanhoPeca, macaY*tamanhoPeca, tamanhoPeca,tamanhoPeca); 
 
         //cobra
-        ctx.fillStyle = "green"; //estilo de preechimento
+        ctx.fillStyle = "black"; //estilo de preechimento
         for (var i = 0; i < rastro.length; i++){
             ctx.fillRect(rastro[i].x*tamanhoPeca, rastro[i].y*tamanhoPeca, tamanhoPeca-1,tamanhoPeca-1);
             if (rastro[i].x == posicaoX && rastro[i].y == posicaoY){ //se a posição do rastro for igual a posição da cobrinha 
@@ -53,7 +56,16 @@ window.onload = function(){
                 calda = 2;
                 pontuacao = 0;
             }
+
+            //if para reiniciar quando bater nas bordas
+            if(rastro[i].x >= quantidadePeca  || rastro[i].x < 0 || rastro[i].y >= quantidadePeca || rastro[i].y < 0){ //se a posição do rastro for maior ou igual a quantidade peça
+                calda = 2;
+                pontuacao = 0;
+                posicaoX = 10;
+                posicaoY = 10;
+         }
         }
+
         //cobrinha andar
         rastro.push({x:posicaoX, y:posicaoY}) //objeto em que vai ter o elemento x (que usou no for) que vai ter a posição atual. o memso para o y
         while (rastro.length > calda){ //se o rastro estiver maior que a calda
@@ -65,8 +77,10 @@ window.onload = function(){
             calda ++; //colocar mais um elemento na calda
             macaX = Math.floor(Math.random()*quantidadePeca); //reposiciona a maçã
             macaY = Math.floor(Math.random()*quantidadePeca); //reposiciona a maçã
+            macaX !== rastro;
+            macaY !== rastro;
             //pontuação
-            pontuacao += 1;
+            pontuacao += 1; //a cada maça pegada aumenta +1 ponto
         }
     }
         //controle da cobrinha
@@ -88,7 +102,24 @@ window.onload = function(){
             case 40: //baixo
                 velocidadeX = 0;
                 velocidadeY = velocidade;
-                break;          
+                break;  
+
+                case 65: //A
+                velocidadeX = -velocidade;
+                velocidadeY = 0;
+                break;
+            case 87: //W
+                velocidadeX = 0;
+                velocidadeY = -velocidade;
+                break;
+            case 68: //D
+                velocidadeX = velocidade;
+                velocidadeY = 0;
+                break;
+            case 83: //S
+                velocidadeX = 0;
+                velocidadeY = velocidade;
+                break; 
             default:
                 
                 break;
